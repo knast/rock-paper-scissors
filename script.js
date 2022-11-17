f7826a888df08a4d1e94b454559a98186cd92ef9
knast <knast808@gmail.com>
2022-11-17 17:22:01 +0100

    Add function whichcompares player's choice with random computer's choice and prompt for the users

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
-    if (randomNumber === 1) {
+    if (randomNumber == 1) {
         return "Rock";
-    } else if (randomNumber === 2) {
+    } else if (randomNumber == 2) {
         return "Paper";
-    } else if (randomNumber === 3) {
+    } else if (randomNumber == 3) {
         return "Scissors";
     }
 }
 
-console.log(getComputerChoice());
+
+function playRound(playersChoice, computersChoice) {
+    if (computersChoice == 'Rock' && playersChoice == 'Paper') {
+        return "You win! Paper beats Rock!";
+    } else if (computersChoice == 'Scissors' && playersChoice == 'Paper') {
+        return 'You lose! Scissors beat Paper!'; 
+    } else if (computersChoice == 'Paper' && playersChoice == 'Paper') {
+        return "It's a draw!";
+    } else if (computersChoice == 'Rock' && playersChoice == 'Scissors') {
+        return "You lose! Rock beats Scissors!";
+    } else if (computersChoice == "Scissors" && playersChoice == 'Scissors') {
+        return "It's a draw!";
+    } else if (computersChoice == "Paper" && playersChoice == "Scissors") {
+        return "You win! Scissors beat Paper!"; 
+    } else if (computersChoice == "Rock" && playersChoice == "Rock") {
+        return "It's a draw!";
+    } else if (computersChoice == "Scissors" && playersChoice == "Rock") {
+        return "You win! Rock beats Scissors";
+    } else if (computersChoice == 'Paper' && playersChoice == "Rock") {
+        return "You lose! Paper beats Rock!";
+    }
+}
+
+let playerChoice = prompt('Choose Rock/Paper/Scissors');
+let finalPlayerChoice = playerChoice.toString();
+let computerChoice = getComputerChoice();
+
+console.log(playRound(playerChoice, computerChoice));
+
